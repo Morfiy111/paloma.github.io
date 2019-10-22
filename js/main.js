33c0d0a02c0425b9c4201f3e948817d8b8b0cd83
morfiy <edurnev@keaz.ru>
2019-10-22 11:21:02 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
-$(document).ready(function () {
-    $(".navbar-toggle").on("click", function () {
-        $(this).toggleClass("active");
-    });
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    margin:0,
+    nav:true,
+    dots:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true
 });
+$('.play').on('click',function(){
+    owl.trigger('autoplay.play.owl',[3000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('autoplay.stop.owl')
+})
