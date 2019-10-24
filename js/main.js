77acd90e31a9a0f7fb27fa6957104d378db26f1d
morfiy <edurnev@keaz.ru>
2019-10-24 11:47:44 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -25,3 +25,25 @@ $(document).ready(function() {
 $('.continue').click(function(){
     $('#burger-content').toggleClass('show');
 });
+
+( function( $ ) {
+    $( document ).ready(function() {
+        $('#menu-left-acide li.has-sub>a').on('click', function(){
+            $(this).removeAttr('href');
+            var element = $(this).parent('li');
+            if (element.hasClass('open')) {
+                element.removeClass('open');
+                element.find('li').removeClass('open');
+                element.find('ul').slideUp();
+            }
+            else {
+                element.addClass('open');
+                element.children('ul').slideDown();
+                element.siblings('li').children('ul').slideUp();
+                element.siblings('li').removeClass('open');
+                element.siblings('li').find('li').removeClass('open');
+                element.siblings('li').find('ul').slideUp();
+            }
+        });
+    });
+} )( jQuery );
